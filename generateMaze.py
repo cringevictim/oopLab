f765073fda89de80873d2440e1226d04a64f9881
AceMuffins <36843738+AceMuffins@users.noreply.github.com>
2022-10-16 11:41:09 +0300

    Update generateMaze.py
    Rewritten to work with Node logic

--- generateMaze.py
+++ generateMaze.py
@@ -1,20 +1,21 @@
 import random
-from Node import Node
+import classNode as cn
+
 
 class generateMaze:
     def __init__(self, size: tuple):
         self.size = size
-        self.maze = [1 for i in range(self.size[0] * self.size[1])]
+        self.maze = [cn.Node((-1, -1)) for i in range(self.size[0] * self.size[1])]
 
     def EdgeWall(self):
         x = self.size[0]
         y = self.size[1]
         for i in range(y):
-            self.maze[i] = 1
-            self.maze[((x - 1) * y) + i] = 1
+            self.maze[i].isWall = 1
+            self.maze[((x - 1) * y) + i].isWall = 1
         for i in range(x):
-            self.maze[i * y] = 1
-            self.maze[i * y + (y - 1)] = 1
+            self.maze[i * y].isWall = 1
+            self.maze[i * y + (y - 1)].isWall = 1
 
     def generate(self):
         x = self.size[0]
@@ -32,23 +33,23 @@ class generateMaze:
             yr = p[2]
             xr = p[3]
 
-            if (self.maze[(xr * y) + yr] == 1):
-                self.maze[(p[1]*y) + p[0]] = 0
-                self.maze[(xr * y) + yr] = 0
+            if self.maze[(xr * y) + yr].isWall == 1:
+                self.maze[(p[1] * y) + p[0]].isWall = 0
+                self.maze[(xr * y) + yr].isWall = 0
 
-                if xr >= 2 and self.maze[((xr-2)*y) + yr] == 1:
+                if xr >= 2 and self.maze[((xr - 2) * y) + yr].isWall == 1:
                     newPassage = [yr, xr - 1, yr, xr - 2]
                     passages.append(newPassage)
 
-                if yr >= 2 and self.maze[(xr*y) + (yr-2)] == 1:
-                    newPassage = [yr-1, xr, yr - 2, xr]
+                if yr >= 2 and self.maze[(xr * y) + (yr - 2)].isWall == 1:
+                    newPassage = [yr - 1, xr, yr - 2, xr]
                     passages.append(newPassage)
 
-                if xr < x - 2 and self.maze[((xr+2)*y) + yr] == 1:
+                if xr < x - 2 and self.maze[((xr + 2) * y) + yr].isWall == 1:
                     newPassage = [yr, xr + 1, yr, xr + 2]
                     passages.append(newPassage)
 
-                if yr < y - 2 and self.maze[(xr * y) + (yr + 2)] == 1:
+                if yr < y - 2 and self.maze[(xr * y) + (yr + 2)].isWall == 1:
                     newPassage = [yr + 1, xr, yr + 2, xr]
                     passages.append(newPassage)
 
