2bacfecc659dbe08750af1407709eef7e43726a3
AceMuffins <36843738+AceMuffins@users.noreply.github.com>
2022-10-16 11:42:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import classNode as cn
+from generateMaze import generateMaze
 
 example = 0
 
 if __name__ == 'main':
+    maze = generateMaze((21, 21)) #Odd numbers only
+    
     instance = cn.Node((1,1))
     print(instance.coordinates)
