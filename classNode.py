d787ca23197826664f8e732224e0305f8f2ea775
AceMuffins <36843738+AceMuffins@users.noreply.github.com>
2022-10-16 11:40:27 +0300

    Update classNode.py
    Maze generation more optimized if all cells start out as walls

--- classNode.py
+++ classNode.py
@@ -1,9 +1,9 @@
 class Node:
     def __init__(self):
-        self.isWall = False
+        self.isWall = True
         self.isPartOfPath = False
         self.coordinates = (-1, -1)
     def __init__(self, coords: tuple):
-        self.isWall = False
+        self.isWall = True
         self.isPartOfPath = False
         self.coordinates = coords
