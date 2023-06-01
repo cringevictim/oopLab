a4c04d2866a75a1183cf5e33ee0b8bcdc33472a0
Ace Muffins <alex_verhola@ukr.net>
2023-06-01 15:24:35 +0300

    Added a simple option to choose maze type via console

--- main.py
+++ main.py
@@ -16,8 +16,22 @@ WIDTH = SIZE_X*TILE
 HEIGHT = SIZE_Y*TILE
 
 if __name__ == '__main__':
-    while True:
+    print("Choose a maze type:\n1 - Primm\n2 - Primm (Complex)\n3 - Binary Tree\n4 - Binary Tree (Complex)")
+    choice = input()
+    while choice > "4" or choice < "0":
+        print("Choose a valid number")
+        choice = input()
+
+    if choice == "1":
+        maze = genMazePrimm((SIZE_X, SIZE_Y))
+    elif choice == "2":
         maze = genMazePrimmComplex((SIZE_X, SIZE_Y))
+    elif choice == "3":
+        maze = genMazeBinaryTree((SIZE_X, SIZE_Y))
+    elif choice == "4":
+        maze = genMazeBinaryTreeComplex((SIZE_X, SIZE_Y))
+
+    while True:
 
         pathF = pathFinder(maze, (1,1), (SIZE_X-2,SIZE_Y-2))
         path = pathF.pathFinder()
